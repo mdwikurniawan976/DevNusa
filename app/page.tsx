a3cefcf2308f7449388e8e0e41ff5bccbf8ec23f
M Dwi Kurniawan <124229813+mdwikurniawan976@users.noreply.github.com>
2025-05-27 14:44:24 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,20 +2,14 @@ import React from "react";
 import Hero from "./components/Home/Hero/Hero";
 import About from "./components/Home/About/About";
 import Feature from "./components/Home/Feature/Feature";
-import Service from "./components/Home/Services/Service";
-import ClientReview from "./components/Home/ClientReview/ClientReview";
-import Team from "./components/Home/Team/Team";
 import Pricing from "./components/Home/Pricing/Pricing";
-import Footer from "./components/Footer/Footer";
+
 
 const Homepage = () => {
   return (
     <div className="overflow-hidden">
       <Hero />
       <About />
-      {/* <Service /> */}
-      {/* <ClientReview />
-      <Team/> */}
       <Pricing/>
       <Feature />
       
