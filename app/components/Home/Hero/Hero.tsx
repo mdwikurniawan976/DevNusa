d05330882cea7c91c2a22d395236843c7d162be1
M Dwi Kurniawan <124229813+mdwikurniawan976@users.noreply.github.com>
2025-05-27 14:35:52 +0700

    Update Hero.tsx

--- app/components/Home/Hero/Hero.tsx
+++ app/components/Home/Hero/Hero.tsx
@@ -1,7 +1,6 @@
 import Image from "next/image";
 import React from "react";
-import { BsStarHalf } from "react-icons/bs";
-import { FaStar } from "react-icons/fa";
+
 
 const Hero = () => {
   return (
