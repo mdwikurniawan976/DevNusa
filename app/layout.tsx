658a20ecb47cd2f54c2627b80b3d32f4cdac2201
M Dwi Kurniawan <124229813+mdwikurniawan976@users.noreply.github.com>
2025-05-27 14:37:54 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Rethink_Sans } from "next/font/google";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import Footer from "./components/Footer/Footer";
